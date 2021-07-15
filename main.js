845d9da60248f6e28d6e77cc08617ed685efeec8
Alan Christian <alancsystem@gmail.com>
2021-07-15 20:07:27 +0000

    Formata codigo

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function consulta_cep() {
-
   // salva o valor digitado do input com o id: cep
   var cep = document.getElementById("cep").value;
 
@@ -8,28 +7,33 @@ function consulta_cep() {
 
   // realiza a consulta pelo cep
   $.ajax({
-
     url: url,
     type: "GET",
 
     // caso a consulta tenha sucesso:
     success: function (response) {
-
       // configura o retorno da busca pelo cep em tela
-      document.getElementById("response").innerHTML = 
-      "Cep: " + response.cep + "<br>" +
-      "Logradouro: " + response.logradouro + "<br>" +
-      "Bairro: " + response.bairro + "<br>" +
-      "Localidade: " + response.localidade + "<br>" +
-      "UF: " + response.uf + "<br>"
+      document.getElementById("response").innerHTML =
+        "Cep: " +
+        response.cep +
+        "<br>" +
+        "Logradouro: " +
+        response.logradouro +
+        "<br>" +
+        "Bairro: " +
+        response.bairro +
+        "<br>" +
+        "Localidade: " +
+        response.localidade +
+        "<br>" +
+        "UF: " +
+        response.uf +
+        "<br>";
 
       // caso o cep nao exista, mostre essa mensagem
-      if(response.cep == undefined) {
-        document.getElementById("response").innerHTML = "Cep não encontrado"
+      if (response.cep == undefined) {
+        document.getElementById("response").innerHTML = "Cep não encontrado";
       }
-
-    }
-
+    },
   });
-
 }
