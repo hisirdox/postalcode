d4c817b58a4ce47213da7424e9c64c09dceffc2c
alan <alancsystem@gmail.com>
2023-04-14 09:58:52 -0300

    melhora aparencia

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 function consulta_cep() {
-  // salva o valor digitado do input com o id: cep
+  // salva o valor digitado no input
   var cep = document.getElementById("cep").value;
-
-  // insere o valor na api de consulta
-  var url = "https://viacep.com.br/ws/" + cep + "/json/";
 
   // realiza a consulta pelo cep
   $.ajax({
-    url: url,
+    url: "https://viacep.com.br/ws/" + cep + "/json/",
     type: "GET",
 
     // caso a consulta tenha sucesso:
     success: function (response) {
       // configura o retorno da busca pelo cep em tela
       document.getElementById("response").innerHTML =
-        "Cep: " +
-        response.cep +
-        "<br>" +
-        "Logradouro: " +
-        response.logradouro +
-        "<br>" +
-        "Bairro: " +
-        response.bairro +
-        "<br>" +
-        "Localidade: " +
-        response.localidade +
-        "<br>" +
-        "UF: " +
-        response.uf +
-        "<br>";
+        "Cep: "        + response.cep        + "<br>" +
+        "Logradouro: " + response.logradouro + "<br>" +
+        "Bairro: "     + response.bairro     + "<br>" +
+        "Localidade: " + response.localidade + "<br>" +
+        "UF: "         + response.uf         + "<br>";
 
       // caso o cep nao exista, mostre essa mensagem
       if (response.cep == undefined) {
